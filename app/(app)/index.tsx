00b7e0b5660c6d88af4a3af2d4ccf79dd2a0d9cf
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-11 16:27:39 -0500

    created basic layout for the app

---
+++ app/(app)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "../../components/Themed";
+
+const ListaProductosViewModel = () => {
+    return (
+        <View style={{flex: 1}}>
+            <Text>Lista Productos</Text>
+        </View>
+    );
+};
+
+export default ListaProductosViewModel;
