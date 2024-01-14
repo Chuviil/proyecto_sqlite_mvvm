70ec9f1fb05f8f1229b39fa3c4be94f8fec0963e
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-13 19:34:08 -0500

    created ListaProductos View

---
+++ views/ListaProductosView.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Text, View} from "../components/Themed";
+import {Producto} from "../models/Producto";
+import {FlatList, SafeAreaView, StyleSheet, TouchableOpacity} from "react-native";
+
+type ListaProductosViewModelType = {
+    productos: Producto[],
+    handleAgregarButton: () => void,
+    handleCardPress: (idProducto: number) => void,
+};
+
+const ListaProductosView = ({viewModel}: { viewModel: ListaProductosViewModelType }) => {
+    return (
+        <SafeAreaView style={styles.flex}>
+            <View>
+                <Text>Lista Productos</Text>
+                <FlatList
+                    data={viewModel.productos}
+                    renderItem={({item}) => (
+                        <TouchableOpacity onPress={() => viewModel.handleCardPress(item.idProducto)} key={item.idProducto}
+                              style={{marginBottom: 10, borderStyle: "solid", borderWidth: 1, borderColor: "white"}}>
+                            <Text>{item.nombre}</Text>
+                            <Text>{item.descripcion}</Text>
+                            <Text>{item.cantidad}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    flex: {
+        flex: 1
+    }
+})
+
+export default ListaProductosView;
