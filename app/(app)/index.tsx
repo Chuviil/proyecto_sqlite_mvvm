36afc06c6711b289fd113548d491291d2725d1f1
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-13 19:35:08 -0500

    created ListaProductosViewModel

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import {Text, View} from "../../components/Themed";
+import React, {useCallback, useState} from 'react';
+import {Producto} from "../../models/Producto";
+import ProductoRepository from "../../services/ProductoRepository";
+import {router, useFocusEffect} from "expo-router";
+import ListaProductosView from "../../views/ListaProductosView";
 
 const ListaProductosViewModel = () => {
-    return (
-        <View style={{flex: 1}}>
-            <Text>Lista Productos</Text>
-        </View>
-    );
+    const [productos, setProductos] = useState<Producto[]>([]);
+
+    const obtenerProductos = async () => {
+        const productosDB = await ProductoRepository.obtenerProductos();
+        setProductos(productosDB);
+    }
+
+    const handleAgregarButton = () => {
+        router.push("/producto/crearProducto");
+    }
+
+    const handleCardPress = (idProducto: number) => {
+        router.push(`/producto/${idProducto}`);
+    }
+
+    useFocusEffect(useCallback(() => {
+        obtenerProductos();
+    }, []));
+
+    const viewModel= {
+        productos,
+        handleAgregarButton,
+        handleCardPress
+    }
+
+    return <ListaProductosView viewModel={viewModel}/>;
 };
 
 export default ListaProductosViewModel;
