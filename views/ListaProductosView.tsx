5802a776cdfc4910773efa4c64de38a7e48c9e32
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-15 06:55:38 -0500

    Updated ListaProductos View to use react native paper

--- views/ListaProductosView.tsx
+++ views/ListaProductosView.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Text, View} from "../components/Themed";
+import {View} from "../components/Themed";
 import {Producto} from "../models/Producto";
 import {FlatList, SafeAreaView, StyleSheet, TouchableOpacity} from "react-native";
+import {Button, Text} from 'react-native-paper';
+import {Stack} from "expo-router";
 
 type ListaProductosViewModelType = {
     productos: Producto[],
@@ -12,17 +14,21 @@ type ListaProductosViewModelType = {
 const ListaProductosView = ({viewModel}: { viewModel: ListaProductosViewModelType }) => {
     return (
         <SafeAreaView style={styles.flex}>
-            <View>
-                <Text>Lista Productos</Text>
-                <TouchableOpacity onPress={viewModel.handleAgregarButton}>
-                    <Text>Agregar Producto</Text>
-                </TouchableOpacity>
+            <Stack.Screen options={{
+                title: "Productos SQLite"
+            }}/>
+            <View style={styles.container}>
+                <Text variant={"headlineLarge"} style={styles.title}>Lista Productos</Text>
+                <Button mode={"contained"} onPress={viewModel.handleAgregarButton}>
+                    Agregar Producto
+                </Button>
                 <FlatList
                     data={viewModel.productos}
                     renderItem={({item}) => (
-                        <TouchableOpacity onPress={() => viewModel.handleCardPress(item.idProducto)} key={item.idProducto}
-                              style={{marginBottom: 10, borderStyle: "solid", borderWidth: 1, borderColor: "white"}}>
-                            <Text>{item.nombre}</Text>
+                        <TouchableOpacity onPress={() => viewModel.handleCardPress(item.idProducto)}
+                                          key={item.idProducto}
+                                          style={styles.card}>
+                            <Text variant={"titleMedium"}>{item.nombre}</Text>
                             <Text>{item.descripcion}</Text>
                             <Text>{item.cantidad}</Text>
                         </TouchableOpacity>
@@ -36,6 +42,21 @@ const ListaProductosView = ({viewModel}: { viewModel: ListaProductosViewModelTyp
 const styles = StyleSheet.create({
     flex: {
         flex: 1
+    },
+    title: {
+        textAlign: "center",
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        gap: 20
+    },
+    card: {
+        marginBottom: 10,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "white",
+        padding: 10
     }
 })
 
